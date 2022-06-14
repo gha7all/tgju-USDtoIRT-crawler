5bac712191c4e9b580442cfa3ff04f23b76cef99
Gha7all <gha7alkh3yr0ddin@gmail.com>
2022-06-15 02:31:41 +0430

    commits prices to the database

--- crawler.py
+++ crawler.py
@@ -1,9 +1,30 @@
+from pydoc import cli
 import time
+import click
 from selenium import webdriver
 import psycopg2
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller as chromedriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import pandas as pd
+
+
+def create_db():
+    cur.execute(
+     'CREATE TABLE prices (id SERIAL PRIMARY KEY, released_date VARCHAR, open_price INT, low_price INT, high_price INT, close_price INT);')
+     
+    conn.commit()
+     
+    
+
+def commit_to_db(open_price, low_price, high_price, close_price, date):
+    cur.execute("""
+                INSERT INTO prices (released_date, open_price, low_price, high_price, close_price) VALUES(%s, %s, %s, %s, %s);
+                """,
+                (date, open_price, low_price, high_price, close_price))
+    conn.commit()
 
 
 def calender_setup():
@@ -17,7 +38,7 @@ def calender_setup():
         from_date = wd.find_element(
             By.ID, 'history-from').send_keys(starting_date)
         to_date = wd.find_element(By.ID, 'history-to').send_keys(last_entered)
-  
+
     last_page_number = wd.find_element(
         By.XPATH, '/html/body/main/div[1]/div[2]/div[2]/div[1]/div/div[3]/div/div[2]/div/div[1]/span/a[6]').text
 
@@ -29,21 +50,15 @@ def click_next():
                               "/html/body/main/div[1]/div[2]/div/div[1]/div/div[3]/div/table/tbody/tr[25]/td[1]")
     wd.execute_script('arguments[0].scrollIntoView(true);', element)
     button = wd.find_element(By.XPATH, '//*[@id="DataTables_Table_0_next"]')
+    time.sleep(1)
     ActionChains(wd).move_to_element(button).click().perform()
-    time.sleep(3)
+    time.sleep(2)
 
 
 def crawler():
     last_page_number = calender_setup()
-
-    for page in range(int(last_page_number)+1):
-        # since the website ignores the first click, we don't miss the first page by clicking on the next page here
-        if page == int(last_page_number):
-            break
-            
-        else:
-            click_next()
-            
+    click_next()
+    for page in range(int(last_page_number) - 1):
         for i in range(1, 31):
             table = wd.find_elements(
                 By.XPATH, f'//*[@id="table-list"]/tr[{i}]')
@@ -67,23 +82,37 @@ def crawler():
                 date = wd.find_element(
                     By.XPATH, f'//*[@id="table-list"]/tr[{i}]/td[8]').text
 
-            print(open_price, low_price, high_price, close_price, date)
+            commit_to_db(open_price=open_price, low_price=low_price, high_price=high_price, close_price=close_price, date=date)
+            # print(date, low_price, high_price, open_price, close_price)
+        click_next()
+
+        
+        
 
 
+conn = psycopg2.connect(host='localhost', database='data',
+                        user='ghazal', password='qwerty')
+
+cur = conn.cursor()
+
+# create_db()  #uncomment if you're running this code for the first time
 
 chromedriver.install()
 chrome_options = webdriver.ChromeOptions()
 wd = webdriver.Chrome(options=chrome_options)
 wd.get('https://www.tgju.org/profile/price_dollar_rl/history')
-wd.maximize_window() 
-wd.implicitly_wait(20) 
+wd.maximize_window()
+wd.implicitly_wait(20)
 crawler()
-# conn = psycopg2.connect(host='localhost', database='data',
-#                         user='ghazal', password='qwerty')
+wd.quit()
 
-# cur = conn.cursor()
-# cur.execute(
-#     'CREATE TABLE prices (id SERIAL PRIMARY KEY, released_date DATE, closed_price INT);')
-# cur.close()
+# cur.execute('SELECT * FROM prices')
 
-# conn.close()
+# df = cur.fetchall()
+# sql = "COPY (SELECT * FROM prices) TO STDOUT WITH CSV DELIMITER ';'"
+# with open("table.csv", "w+") as file:
+#     cur.copy_expert(sql, file)
+
+cur.close()
+
+conn.close()
